6bc09b886ead39bc575877129ea9036e289d6a07
NickSwitchSchool <97087180@st.deltion.nl>
2022-11-18 13:11:31 +0100

    rollsound

--- Minigolf/Assets/Scripts/SoundsTest.cs
+++ Minigolf/Assets/Scripts/SoundsTest.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 
-public class FootstepSoundsTest : MonoBehaviour
+public class SoundsTest : MonoBehaviour
 {
+    [Header("Footsteps")]
     [Header("Import these to the player movement script")]
     [SerializeField] private GameObject[] footstepsFX;
     [SerializeField] private float footstepDelay;
     private float timer;
-    [Space(20)]
+    [Space(8)]
     [Header("This bool is for testing purposes")]
     public bool walking;
+    [Space(20)]
+    [Header("Golfball roll")]
+    [Header("Import these to the golfball script")]
+    [SerializeField] AudioSource rollAudio;
+    private bool rollSoundTriggered;
+    [Space(8)]
+    [Header("This bool is for testing purposes")]
+    public bool rolling;
 
     private void Update()
     {
@@ -28,6 +37,19 @@ public class FootstepSoundsTest : MonoBehaviour
         else
         {
             timer = 0;
+        }
+
+        //play roll sound when the ball is rolling
+        //replace the rolling bool with a condition that is triggered when the ball is rolling
+        if (rolling && !rollSoundTriggered)
+        {
+            rollAudio.Play();
+            rollSoundTriggered = true;
+        }
+        else if (!rolling && rollSoundTriggered)
+        {
+            rollAudio.Pause();
+            rollSoundTriggered = false;
         }
     }
 }

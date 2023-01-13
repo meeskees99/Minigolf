46251559a8cda2e1e3ac39514159dc07dd2d91f0
LusseveldNiek <97088465@st.deltion.nl>
2023-01-13 12:52:47 +0100

    elevator

--- Minigolf/Assets/Scripts/elevator.cs
+++ Minigolf/Assets/Scripts/elevator.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class elevator : MonoBehaviour
 {
-    private float distance;
     public float turnSpeed;
     public Quaternion enemyRotation;
     public Transform[] waypoints;
     private int waypointIndex;
+    private bool ballInElevator;
+    public float timer;
+    public GameObject door;
+    public float doorSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
-        if (Vector3.Distance(transform.position, waypoints[waypointIndex].position) < 0.1f)
+        if(ballInElevator)
         {
-            waypointIndex++;
-            enemyRotation = Quaternion.LookRotation(waypoints[waypointIndex].transform.position - transform.position);
+            door.transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(5, 1, 1), doorSpeed);
+            if (Vector3.Distance(transform.position, waypoints[waypointIndex].position) < 0.1f && waypointIndex != 3)
+            {
+                waypointIndex++;
+                enemyRotation = Quaternion.LookRotation(waypoints[waypointIndex].transform.position - transform.position);
+            }
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].position, 1 * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, enemyRotation, Time.deltaTime * turnSpeed);
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].position, 1 * Time.deltaTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, enemyRotation, Time.deltaTime * turnSpeed);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "golfBall")
+        {
+            timer += Time.deltaTime;
+            if(timer > 1)
+            {
+                ballInElevator = true;
+            }
+        }
     }
 }

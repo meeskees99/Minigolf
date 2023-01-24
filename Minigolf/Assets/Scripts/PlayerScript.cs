fd797352a0964e4705adeeec17c329d43538a5c3
NickSwitchSchool <97087180@st.deltion.nl>
2023-01-24 16:10:56 +0100

    hehe ik ben een mongool

--- Minigolf/Assets/Scripts/PlayerScript.cs
+++ Minigolf/Assets/Scripts/PlayerScript.cs
@@ -50,7 +50,7 @@ public class PlayerScript : MonoBehaviour
 
         //compass points to the ball
         compass.SetActive(lookingForBall);
-        //hitsPaper.SetActive(!lookingForBall);
+        hitsPaper.SetActive(!lookingForBall);
         if (lookingForBall)
         {
             Vector3 targetDirection = compass.transform.position - ball.transform.position;
@@ -61,7 +61,7 @@ public class PlayerScript : MonoBehaviour
         else
         {
             Debug.Log(GolfHitScript.ballHitCounter);
-            //hitsDisplay.text = GolfHitScript.ballHitCounter.ToString();
+            hitsDisplay.text = GolfHitScript.ballHitCounter.ToString();
         }
 
         if (Physics.Raycast(playerOrgin.transform.position, -playerOrgin.transform.up, out checkpointHit, 1) && checkpointHit.transform.gameObject.tag != "Boundary")
